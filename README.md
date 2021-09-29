a486ac0073851617212837114bb46aec44f6ad76
careman <careman@inbox.ru>
2021-09-29 13:42:59 +0300

    changed output from csv to table
    used beautifultable

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Progmam converts poorly written Roman integers to well-written
 
+used beautifultable for printing to .txt formatting
+
--- roman_main.py
+++ roman_main.py
@@ -1,5 +1,8 @@
+from os import path
+from numpy import character, number
 import pandas as pd
 from roman_class import RomanInt
+from prettytable import PrettyTable
 
 with open('p089_roman.txt', 'r') as file:
     input_roman = file.readlines()
@@ -10,9 +13,24 @@ output_arabic = [RomanInt(roman_int).integer for roman_int in input_roman]
 
 df = pd.DataFrame({'Input Roman':input_roman, 'Output Arabic': output_arabic, 'Output Roman': output_roman})
 df['True'] =df['Input Roman'] != df['Output Roman']
-level_map = {True: 'changed', False: None}
+level_map = {True: 'changed', False: ''}
 df['Changed'] = df['True'].map(level_map)
+df['Output Arabic'] = df['Output Arabic'].apply(str)
 
 df = df[['Input Roman', 'Output Arabic', 'Output Roman', 'Changed']]
 
-df.to_csv('roman_out.txt', index=False, columns=None)
+df_2 = PrettyTable(border = False, header = False)
+df_2.add_rows(df.values)
+df_2.field_names = df.columns
+print(df_2)
+
+with open('roman_out.txt', 'w') as file:
+
+    df_2 = df_2.get_string()
+    file.write(df_2)
+    file.write('\n')
+
+
+
+
+
